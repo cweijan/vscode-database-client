ad364ff9b1e59ab11e254a1646c30b911ba1804d
cweijan <cweijan@163.com>
2020-04-27 23:33:13 +0800

    fix edit connection fail

--- src/service/connect/impl/mysqlConnectService.ts
+++ src/service/connect/impl/mysqlConnectService.ts
@@ -4,6 +4,11 @@ import { Node } from "../../../model/interface/node";
 
 export class MysqlConnectService extends AbstractConnectService {
     protected async connect(connectionNode: Node): Promise<void> {
+        const connectId = connectionNode.getConnectId();
+        const connection = ConnectionManager.getActiveConnectByKey(connectId)
+        if (connection) {
+            ConnectionManager.removeConnection(connectId)
+        }
         await ConnectionManager.getConnection(connectionNode)
     }
 
