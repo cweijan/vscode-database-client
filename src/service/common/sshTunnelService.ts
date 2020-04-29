4b004cdea6b47682578ca6b3d8266c73dff5070b
cweijan <cweijan@163.com>
2020-04-29 09:15:43 +0800

    fix create tunnel bug

--- src/service/common/sshTunnelService.ts
+++ src/service/common/sshTunnelService.ts
@@ -1,16 +1,16 @@
 import tunnel = require('tunnel-ssh')
-import * as getPort from 'get-port'
 import { Node } from '../../model/interface/node';
 import { Console } from '../../common/outputChannel';
 import { existsSync } from 'fs';
+import * as portfinder from 'portfinder'
 
 export class SSHTunnelService {
 
-    private tunelMark: { [key: string]: any } = {};
+    private tunelMark: { [key: string]: { tunnel: any, tunnelPort: number } } = {};
 
     public closeTunnel(connectId: string) {
         if (this.tunelMark[connectId]) {
-            this.tunelMark[connectId].close()
+            this.tunelMark[connectId].tunnel.close()
             delete this.tunelMark[connectId]
         }
     }
@@ -18,15 +18,13 @@ export class SSHTunnelService {
     public createTunnel(connectionNode: Node, errorCallback: (error) => void): Promise<Node> {
         return new Promise(async (resolve) => {
             const ssh = connectionNode.ssh
-            if (!connectionNode.ssh.tunnelPort) {
-                connectionNode.ssh.tunnelPort = await getPort({ port: 10567 })
-            }
-            const port = connectionNode.ssh.tunnelPort;
             const key = connectionNode.getConnectId();
             if (this.tunelMark[key]) {
-                resolve({ ...connectionNode, port } as Node)
+                resolve({ ...connectionNode, port: this.tunelMark[key].tunnelPort } as Node)
                 return;
             }
+            const port = await portfinder.getPortPromise();
+            connectionNode.ssh.tunnelPort = port
             const config = {
                 username: ssh.username,
                 password: ssh.password,
@@ -46,7 +44,7 @@ export class SSHTunnelService {
             };
 
             const localTunnel = tunnel(config, (error, server) => {
-                this.tunelMark[key] = localTunnel
+                this.tunelMark[key] = { tunnel: localTunnel, tunnelPort: port }
                 if (error && errorCallback) {
                     delete this.tunelMark[key]
                     errorCallback(error)
