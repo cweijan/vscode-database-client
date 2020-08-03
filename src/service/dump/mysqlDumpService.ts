6110db00bc202b695ff192621d8a21a83de7d02b
cweijan <cweijan@163.com>
2020-08-03 17:10:35 +0800

    enhance dump speed.

--- src/service/dump/mysqlDumpService.ts
+++ src/service/dump/mysqlDumpService.ts
@@ -10,15 +10,21 @@ import path = require('path');
 
 export class MysqlDumpService extends AbstractDumpService {
     protected dumpData(node: Node, exportPath: string, withData: boolean): void {
-        Console.log(`Doing backup ${node.host}_${node.database}...`);
+
+        const port = node.usingSSH ? NodeUtil.getTunnelPort(node.getConnectId()) : node.port;
         const tableName = node instanceof TableNode ? node.table : null;
+        const dumpFilePath = path.join(exportPath, `${node.database}${tableName ? "_" + tableName : ''}_${format('yyyy-MM-dd_hhmmss', new Date())}.sql`);
+
+        Console.log(`${format('yyyy-MM-dd hh:mm:ss', new Date())} : Doing backup ${node.host}_${node.database}...`);
+        Console.log(`You can run command \`mysqldump -h ${node.host} -P ${port} -u ${node.user} -p --database ${node.database} > ${dumpFilePath}\` to speed up dump.`);
+
         const option: Options = {
             connection: {
                 host: node.host,
                 user: node.user,
                 password: node.password,
                 database: node.database,
-                port: node.usingSSH ? NodeUtil.getTunnelPort(node.getConnectId()) : node.port,
+                port: port,
             },
             dump: {
                 tables: tableName ? [tableName] : [],
@@ -31,17 +37,22 @@ export class MysqlDumpService extends AbstractDumpService {
                     }
                 },
             },
-            dumpToFile: path.join(exportPath, `${node.database}${tableName ? "_" + tableName : ''}_${format('yyyy-MM-dd_hhmmss', new Date())}.sql`),
+            dumpToFile: dumpFilePath,
         };
         if (!withData) {
             option.dump.data = false;
+        } else {
+            option.dump.data = {
+                format: false,
+                maxRowsPerInsertStatement:5000
+            }
         }
         mysqldump(option).then(() => {
             vscode.window.showInformationMessage(`Backup ${node.getHost()}_${node.database} success!`);
         }).catch((err) => {
             vscode.window.showErrorMessage(`Backup ${node.getHost()}_${node.database} fail!\n${err}`);
         }).then(() => {
-            Console.log("backup end.");
+            Console.log(`${format('yyyy-MM-dd hh:mm:ss', new Date())} : backup end.`);
         })
     }
 
