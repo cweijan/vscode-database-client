138370eabb73e8d9a233af056e2999624fc77478
cweijan <cweijan@163.com>
2021-01-04 10:42:05 +0800

    Show count when size is zero.

--- src/vue/result/App.vue
+++ src/vue/result/App.vue
@@ -20,7 +20,7 @@
         <el-button @click="exportOption.visible = true" type="primary" size="mini" icon="el-icon-bottom" circle title="Export"></el-button>
         <el-button type="success" size="mini" icon="el-icon-caret-right" title="Execute Sql" circle @click='info.visible = false;execute(toolbar.sql);'></el-button>
         <div style="display:inline-block">
-          <el-pagination @size-change="changePageSize" @current-change="page=>changePage(page,true)" @next-click="()=>changePage(1)" @prev-click="()=>changePage(-1)" :current-page.sync="page.pageNum" :small="true" :page-size="page.pageSize" :page-sizes="[100,200,300,400,500,1000]" :layout="page.total?'sizes,prev,pager, next, total, jumper':'sizes,prev, next, jumper'" :total="page.total">
+          <el-pagination @size-change="changePageSize" @current-change="page=>changePage(page,true)" @next-click="()=>changePage(1)" @prev-click="()=>changePage(-1)" :current-page.sync="page.pageNum" :small="true" :page-size="page.pageSize" :page-sizes="[100,200,300,400,500,1000]" :layout="page.total!=null?'sizes,prev,pager, next, total, jumper':'sizes,prev, next, jumper'" :total="page.total">
           </el-pagination>
         </div>
       </div>
