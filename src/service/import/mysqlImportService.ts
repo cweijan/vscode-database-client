6762e7392da14cf429f8e0e7623c51ea96c2a077
cweijan <cweijan@163.com>
2021-01-12 18:10:13 +0800

    Support import sql when mysql command not found.

--- src/service/import/mysqlImportService.ts
+++ src/service/import/mysqlImportService.ts
@@ -3,27 +3,30 @@ import { exec } from "child_process";
 import { Console } from "../../common/Console";
 import { NodeUtil } from "../../model/nodeUtil";
 import { ImportService } from "./importService";
+var commandExistsSync = require('command-exists').sync;
 
-
-export class MysqlImportService implements ImportService {
+export class MysqlImportService extends ImportService {
 
     public importSql(importPath: string, node: Node): void {
 
-        NodeUtil.of(node)
+        if (commandExistsSync('mysql')) {
+            NodeUtil.of(node)
+            const host = node.usingSSH ? "127.0.0.1" : node.host
+            const port = node.usingSSH ? NodeUtil.getTunnelPort(node.getConnectId()) : node.port;
+            const command = `mysql -h ${host} -P ${port} -u ${node.user} ${node.password ? `-p${node.password}` : ""} ${node.database || ""} < ${importPath}`
+            Console.log(` Executing: \`${command.replace(/-p.+? /, "-p****** ")}\``);
+            exec(command, err => {
+                if (err) {
+                    Console.log(err);
+                } else {
+                    Console.log(`Import Success!`);
+                }
+            })
+        } else {
+            super.importSql(importPath,node)
+        }
 
-        const host = node.usingSSH ? "127.0.0.1" : node.host
-        const port = node.usingSSH ? NodeUtil.getTunnelPort(node.getConnectId()) : node.port;
-        const command = `mysql -h ${host} -P ${port} -u ${node.user} ${node.password ? `-p${node.password}` : ""} ${node.database||""} < ${importPath}`
-        Console.log(`
-        Ensure you has been install mysql.\n
-        Executing: \`${command.replace(/-p.+? /, "-p****** ")}\``);
-        exec(command, err => {
-            if (err) {
-                Console.log(err);
-            } else {
-                Console.log(`Import Success!`);
-            }
-        })
+
     }
 
 }
