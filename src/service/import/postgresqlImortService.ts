4c1cf20380cd79c8e4c0e399f2c0014eb83a84c8
cweijan <cweijan@163.com>
2021-02-02 18:37:51 +0800

    Init postgresql import service.

--- src/service/import/postgresqlImortService.ts
+++ src/service/import/postgresqlImortService.ts
@@ -1,5 +1,29 @@
+import { Console } from "@/common/Console";
+import { Node } from "@/model/interface/node";
+import { NodeUtil } from "@/model/nodeUtil";
+import { exec } from "child_process";
 import { ImportService } from "./importService";
+var commandExistsSync = require('command-exists').sync;
 
-export class PostgresqlImortService extends ImportService{
+export class PostgresqlImortService extends ImportService {
+    public importSql(importPath: string, node: Node): void {
 
+        if (commandExistsSync('pg_restore')) {
+            NodeUtil.of(node)
+            const host = node.usingSSH ? "127.0.0.1" : node.host
+            const port = node.usingSSH ? NodeUtil.getTunnelPort(node.getConnectId()) : node.port;
+            const command = `SET "PGPASSWORD=${node.password}" && pg_restore -h ${host} -p ${port} -U ${node.user} -d ${node.database} ${importPath}`
+            Console.log(` Executing: \`${command}`);
+            exec(command, err => {
+                if (err) {
+                    Console.log(err);
+                } else {
+                    Console.log(`Import Success!`);
+                }
+            })
+        } else {
+            super.importSql(importPath, node)
+        }
+
+    }
 }
