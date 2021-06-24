6a958555d2a3c814dfbd9cee9961c2d9b22041d7
cweijan <cweijan@163.com>
2021-06-24 11:34:43 +0800

    Enhance sql detect.

--- src/provider/codelen/sqlCodeLensProvider.ts
+++ src/provider/codelen/sqlCodeLensProvider.ts
@@ -6,7 +6,7 @@ import * as vscode from 'vscode';
 
 export class SqlCodeLensProvider implements vscode.CodeLensProvider {
 
- 
+
     onDidChangeCodeLenses?: vscode.Event<void>;
     provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CodeLens[]> {
         return this.parseCodeLens(document)
@@ -27,75 +27,63 @@ export class SqlCodeLensProvider implements vscode.CodeLensProvider {
 
         const delimter = this.getDelimter();
 
-        /**
-         * TODO
-         * 1. if string include delimter
-         * 2. if one line include two sql.
-         */
-
         const codeLens: vscode.CodeLens[] = []
+        const context = { inSingleQuoteString: false, inDoubleQuoteString: false, inComment: false, sql: '', start: null }
 
-        let start: vscode.Position;
-        let end: vscode.Position;
-        let sql: string = "";
-        let inBlockComment = false;
-        const lineCount = Math.min(document.lineCount, 3000);
+        const lineCount = Math.min(document.lineCount, 5000);
         for (var i = 0; i < lineCount; i++) {
-            let col = 0;
-            var originText = document.lineAt(i).text
-            var text = originText?.replace(/(--|#).+/, '')
-                ?.replace(/\/\*.*?\*\//g, '')
-            if (inBlockComment) {
-                const blockEndMatch = text.match(/.*?\*\//)
-                if (!blockEndMatch) {
+            var text = document.lineAt(i).text
+            for (let j = 0; j < text.length; j++) {
+                const ch = text.charAt(j);
+                // comment check
+                if (ch == '*' && text.charAt(j + 1) == '/') {
+                    j++;
+                    context.inComment = false;
                     continue;
                 }
-                inBlockComment = false;
-                text = text.replace(/.*?\*\//, '')
-                col = blockEndMatch[0].length
-            } else {
-                inBlockComment = text.match(/\/\*/) != null
-                if (inBlockComment) {
-                    text = text.replace(/\/\*.*?/, '')
+                if (context.inComment) continue;
+                // string check
+                if (ch == `'`) {
+                    context.inSingleQuoteString = !context.inSingleQuoteString;
+                    continue;
+                } else if (ch == `"`) {
+                    context.inDoubleQuoteString = !context.inDoubleQuoteString;
+                    continue;
+                }
+                if (context.inSingleQuoteString || context.inDoubleQuoteString) continue;
+                // line comment
+                if (ch == '-' && text.charAt(j + 1) == '-') break;
+                // block comment start
+                if (ch == '/' && text.charAt(j + 1) == '*') {
+                    j++;
+                    context.inComment = true;
+                    continue;
+                }
+                // check sql end 
+                if (ch == delimter) {
+                    if (!context.start) continue;
+                    const range = new vscode.Range(context.start, new vscode.Position(i, j + 1));
+                    if (current && (range.contains(current) || range.start.line > current.line)) {
+                        return context.sql;
+                    }
+                    codeLens.push(new vscode.CodeLens(range, { command: "mysql.codeLens.run", title: "▶ Run SQL", arguments: [context.sql], }));
+                    context.sql = ''
+                    context.start = null
+                    continue;
+                }
+
+                if (!context.start) {
+                    context.start = new vscode.Position(i, j)
+                }
+                context.sql = context.sql + ch;
+                // if end withtout delimter
+                if (i == lineCount - 1 && j == text.length - 1) {
+                    const range = new vscode.Range(context.start, new vscode.Position(i, j + 1));
+                    if (current) return context.sql;
+                    codeLens.push(new vscode.CodeLens(range, { command: "mysql.codeLens.run", title: "▶ Run SQL", arguments: [context.sql], }));
                 }
             }
 
-            text=text.trim()
-            if (text == '') continue;
-
-            if (text && !start) {
-                start = new vscode.Position(i, col)
-            }
-
-            let sep = text.indexOf(delimter)
-            if (start && (lineCount - 1 == i)) {
-                sep = text.length;
-            }
-
-            sql = sql + "\n" + (sep != -1 ? text.substr(0, sep) : text);
-
-            if (sep != -1) {
-                end = new vscode.Position(i, sep)
-                const range = new vscode.Range(start, end);
-                if (current && (range.contains(current) || range.start.line > current.line)) {
-                    return sql;
-                }
-                codeLens.push(new vscode.CodeLens(range, {
-                    command: "mysql.codeLens.run",
-                    title: "▶ Run SQL",
-                    arguments: [sql],
-                }));
-                start = null;
-                sql = text.substr(sep + delimter.length)
-                continue;
-            }
-        }
-
-        if (current) {
-            if (codeLens.length > 0) {
-                return codeLens[codeLens.length - 1].command.arguments[0]
-            }
-            return document.getText()
         }
 
         return codeLens
