5846bbe35363b9af7c8b360e40bb1d1c16db74ec
cweijan <cweijan@163.com>
2021-07-13 21:35:42 +0800

    Fix set variable fail on linux #248.

--- src/model/interface/node.ts
+++ src/model/interface/node.ts
@@ -8,6 +8,7 @@ import { ConnectionManager } from "@/service/connectionManager";
 import { SqlDialect } from "@/service/dialect/sqlDialect";
 import { QueryUnit } from "@/service/queryUnit";
 import { ServiceManager } from "@/service/serviceManager";
+import { platform } from "node:os";
 import * as vscode from "vscode";
 import { Memento } from "vscode";
 var commandExistsSync = require('command-exists').sync;
@@ -285,7 +286,8 @@ export abstract class Node extends vscode.TreeItem implements CopyAble {
             command = `mysql -u ${this.user} -p${this.password} -h ${this.host} -P ${this.port} \n`;
         } else if (this.dbType == DatabaseType.PG) {
             this.checkCommand('psql');
-            command = `set "PGPASSWORD=${this.password}" && psql -U ${this.user} -h ${this.host} -p ${this.port} -d ${this.database} \n`;
+            let prefix = platform() == 'win32' ? 'set' : 'export';
+            command = `${prefix} "PGPASSWORD=${this.password}" && psql -U ${this.user} -h ${this.host} -p ${this.port} -d ${this.database} \n`;
         } else if (this.dbType == DatabaseType.REDIS) {
             this.checkCommand('redis-cli');
             command = `redis-cli -h ${this.host} -p ${this.port} \n`;
@@ -294,7 +296,7 @@ export abstract class Node extends vscode.TreeItem implements CopyAble {
             command = `mongo --host ${this.host} --port ${this.port} ${this.user && this.password ? ` -u ${this.user} -p ${this.password}` : ''} \n`;
         } else if (this.dbType == DatabaseType.SQLITE) {
             command = `${getSqliteBinariesPath()} ${this.dbPath} \n`;
-        } else{
+        } else {
             vscode.window.showErrorMessage(`Database type ${this.dbType} not support open terminal.`)
             return;
         }
--- src/service/import/postgresqlImortService.ts
+++ src/service/import/postgresqlImortService.ts
@@ -2,6 +2,7 @@ import { Console } from "@/common/Console";
 import { Node } from "@/model/interface/node";
 import { NodeUtil } from "@/model/nodeUtil";
 import { exec } from "child_process";
+import { platform } from "os";
 import { ImportService } from "./importService";
 var commandExistsSync = require('command-exists').sync;
 
@@ -14,7 +15,8 @@ export class PostgresqlImortService extends ImportService {
             const port = node.usingSSH ? NodeUtil.getTunnelPort(node.getConnectId()) : node.port;
             const command = `psql -h ${host} -p ${port} -U ${node.user} -d ${node.database} < ${importPath}`
             Console.log(`Executing: ${command}`);
-            exec(`set "PGPASSWORD=${node.password}" && ${command}`, (err,stdout,stderr) => {
+            let prefix = platform() == 'win32' ? 'set' : 'export';
+            exec(`${prefix} "PGPASSWORD=${node.password}" && ${command}`, (err,stdout,stderr) => {
                 if (err) {
                     Console.log(err);
                 }else if(stderr){
