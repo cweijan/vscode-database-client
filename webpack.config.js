fe5318e3fbe838a5e29e64674be2b3e6aa9df9ff
cweijan <cweijan@163.com>
2021-08-13 09:34:37 +0800

    Trim warning.

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,9 @@ module.exports = [
         plugins: [
             new webpack.IgnorePlugin(/^(pg-native|cardinal|encoding|aws4)$/)
         ],
+        stats: {
+            warningsFilter: [/critical dependency:/i],
+        },
         module: { rules: [{ test: /\.ts$/, exclude: /(node_modules|bin)/, use: ['ts-loader'] }] },
         optimization: { minimize: isProd },
         watch: !isProd,
