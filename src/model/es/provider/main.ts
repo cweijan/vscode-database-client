f70710b25da51ad4453093accad65828c218dcf3
cweijan <cweijan@163.com>
2021-09-17 16:26:52 +0800

    Add shortcut to run es query.

--- src/model/es/provider/main.ts
+++ src/model/es/provider/main.ts
@@ -5,6 +5,7 @@ import { DocumentFinder } from './documentFinder';
 import { ElasticCodeLensProvider } from './ElasticCodeLensProvider';
 import { ElasticCompletionItemProvider } from './ElasticCompletionItemProvider';
 import { ElasticMatch } from './ElasticMatch';
+import { ElasticMatches } from './ElasticMatches';
 
 export async function activeEs(context: vscode.ExtensionContext) {
     const languages = { language: 'es' };
@@ -12,6 +13,7 @@ export async function activeEs(context: vscode.ExtensionContext) {
         vscode.languages.registerCompletionItemProvider(languages, new ElasticCompletionItemProvider(), '/', '?', '&', '"'),
         vscode.languages.registerCodeLensProvider(languages, new ElasticCodeLensProvider(context)),
         vscode.commands.registerCommand('mysql.elastic.document', (em: ElasticMatch) => { DocumentFinder.open(em.Path.Text) }),
+        vscode.commands.registerCommand('mysql.runES', runES),
         vscode.commands.registerCommand('mysql.elastic.execute', EsUtil.executeEsQueryFile),
         vscode.commands.registerCommand('mysql.elastic.lint', (em: ElasticMatch) => {
             if (em && em.HasBody) {
@@ -21,4 +23,17 @@ export async function activeEs(context: vscode.ExtensionContext) {
             }
         }),
     );
+}
+
+function runES(){
+    if (!vscode.window.activeTextEditor)
+        return;
+    const esMatches = new ElasticMatches(vscode.window.activeTextEditor);
+    for (const esMatche of esMatches.Matches) {
+        if (esMatche.Range.contains(vscode.window.activeTextEditor.selection.active)) {
+            EsUtil.executeEsQueryFile(esMatche, false);
+            return;
+        }
+    }
+    vscode.window.showErrorMessage("Not elastic search query found!");
 }
