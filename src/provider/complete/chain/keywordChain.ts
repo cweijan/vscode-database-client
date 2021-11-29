3de766922723a135cbfcd56bf3ff0891f08851f2
cweijan <cweijan@163.com>
2021-11-29 10:44:21 +0800

    Update sql template.

--- src/provider/complete/chain/keywordChain.ts
+++ src/provider/complete/chain/keywordChain.ts
@@ -5,7 +5,7 @@ export class KeywordChain implements ComplectionChain {
 
     private keywordList: string[] = ["SCHEMA","JOIN", "AND", "OR", "SELECT", "SET", "UPDATE", "DELETE", "TABLE", "INSERT", "INTO", "VALUES", "FROM", "WHERE", "IS","NULL","DATABASE",
         "GROUP BY", "ORDER BY", "HAVING", "LIMIT", "ALTER", "CREATE", "DROP", "FUNCTION", "CASE", "PROCEDURE", "TRIGGER", "INDEX", "CHANGE", "COLUMN", "BETWEEN","RLIKE",
-        "ADD", 'SHOW', "PRIVILEGES", "IDENTIFIED", "VIEW", "CURSOR", "EXPLAIN", "ROLLBACK", "COMMENT", "COMMIT", "BEGIN", "DELIMITER", "CALL", "REPLACE","TEMPORARY",
+        "ADD", 'SHOW', "PRIVILEGES", "IDENTIFIED", "VIEW", "CURSOR", "EXPLAIN", "ROLLBACK", "COMMENT", "COMMIT", "BEGIN", "DELIMITER", "CALL", "REPLACE","TEMPORARY","COLLATE",
         "REFERENCES", "USING", "END", "BEFORE", "AFTER", "GRANT", "RETURNS", "SOME", "ANY", "ASC", "DESC", "UNIQUE", "UNION", "ALL", "ON","REGEXP","MATERIALIZED",
         "OUTER", "INNER", "EXEC", "EXISTS", "NOT", "FOREIGN", "FULL", "LIKE", "IN", "PRIMARY", "KEY", "RIGHT", "LEFT", "TRUNCATE", "IGNORE", "DISTINCT","SOURCE"];
     private keywordComplectionItems: vscode.CompletionItem[] = [];
