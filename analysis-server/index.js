f44357da52fef3e68ddead1264df0e2a2e8711f1
cweijan <cweijan@163.com>
2022-01-26 14:07:23 +0800

    增加返回ip功能

--- analysis-server/index.js
+++ analysis-server/index.js
@@ -8,25 +8,29 @@ const format = require('date-format');
 
 const con = mysql.createConnection('mysql://root:root@127.0.0.1:3306/test')
 
-app.use(cors()).use(bodyParser.text({ type: "*/*" })).post("/a", (req, res) => {
-    const body = req.body;
-    try {
-        const dec = JSON.parse(decrypt(body))
-        const ip = req.socket.remoteAddress;
-        console.log(JSON.stringify(dec))
-        con.query(`insert into user(create_time,ip,user_name,platform,version,info,ext,git_name) values
-        ('${format('yyyy-MM-dd hh:mm:ss', new Date())}','${(ip||'').replace('::ffff:', '')}',
+app.use(cors()).use(bodyParser.text({ type: "*/*" }))
+    .get("/", (req, res) => {
+        res.send(req.socket.remoteAddress)
+    })
+    .post("/a", (req, res) => {
+        const body = req.body;
+        try {
+            const dec = JSON.parse(decrypt(body))
+            const ip = req.socket.remoteAddress;
+            console.log(JSON.stringify(dec))
+            con.query(`insert into user(create_time,ip,user_name,platform,version,info,ext,git_name) values
+        ('${format('yyyy-MM-dd hh:mm:ss', new Date())}','${(ip || '').replace('::ffff:', '')}',
         '${trim(dec.u)}','${dec.p}','${dec.v}','${trim(dec.i)}','${dec.e}','${trim(dec.g)}')`)
-        res.json({ s: true, ip })
-    } catch (error) {
-        console.log(error)
-        res.json({})
-    }
+            res.json({ s: true, ip })
+        } catch (error) {
+            console.log(error)
+            res.json({})
+        }
 
-})
+    })
 
 function trim(str) {
-    if(!str)return ''
+    if (!str) return ''
     return str.replace(/'/g, '\'\'')
 }
 
